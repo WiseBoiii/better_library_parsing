b3ee2473e8a1d77b5ac3d155802e094858fb944e
WiseBoiii <aleksashin.1001@gmail.com>
2024-08-03 17:36:55 +0300

    Update parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -14,7 +14,7 @@ def main():
     url_pattern = 'https://tululu.org/l55/'
     book_url_pattern = 'https://tululu.org/'
     downloading_url = 'https://tululu.org/txt.php'
-    fantastic_books = []
+    archive_fantastic_books = []
     fantastic_parser = argparse.ArgumentParser(
         description='Это программа является парсером бесплатной онлайн-библиотеки Tululu'
     )
@@ -30,20 +30,21 @@ def main():
                                   help='Параметр, отвечающий за выбор директории',
                                   type=str, default='parsed_result/')
     fantastic_args = fantastic_parser.parse_args()
-    for page in range(fantastic_args.start_page, fantastic_args.end_page):
-        url = f'{url_pattern}{page}'
+    for page_number in range(fantastic_args.start_page, fantastic_args.end_page):
+        url = f'{url_pattern}{page_number}'
         try:
             page_response = requests.get(url)
             page_response.raise_for_status()
             soup = BeautifulSoup(page_response.text, 'lxml')
-            all_fantastic_book_ids = soup.select('.d_book')
-            for fantastic_book in all_fantastic_book_ids:
-                fantastic_book_id = fantastic_book.select_one('a')['href']
-                param_id = int(fantastic_book_id.replace('/', '').lstrip('b'))
+            fantastic_books = soup.select('.d_book')
+            for fantastic_book in fantastic_books:
+                fantastic_book_url = fantastic_book.select_one('a')['href']
+                book_id = int(fantastic_book_url.replace('/', '').lstrip('b'))
                 params = {
-                    'id': param_id
+                    'id': book_id
                 }
-                fantastic_book_link = urllib.parse.urljoin(book_url_pattern, fantastic_book_id)
+                fantastic_book_link = urllib.parse.urljoin(url_pattern, fantastic_book_url)
+                print(fantastic_book_link)
                 fantastic_book_response = requests.get(fantastic_book_link)
                 fantastic_book_response.raise_for_status()
                 parsed_fantastic_book = parse_book_page(fantastic_book_link, fantastic_book_response)
@@ -64,14 +65,14 @@ def main():
                     'comments': parsed_fantastic_book['comments'],
                     'genres': parsed_fantastic_book['genres']
                 }
-                fantastic_books.append(extended_parsed_fantastic_book)
+                archive_fantastic_books.append(extended_parsed_fantastic_book)
         except requests.exceptions.HTTPError:
             print('Такой книги не существует')
         except requests.ConnectionError:
             print('Проблемы с соединением. Идет переподключение...')
             time.sleep(5)
 
-    all_about_fantastic_books = json.dumps(fantastic_books, ensure_ascii=False).encode('utf-8')
+    all_about_fantastic_books = json.dumps(archive_fantastic_books, ensure_ascii=False).encode('utf-8')
     with open(f'{fantastic_args.dest_folder}/all_about_fantastic_books.json', 'wb') as fantastic_book_file:
         fantastic_book_file.write(all_about_fantastic_books)
 
