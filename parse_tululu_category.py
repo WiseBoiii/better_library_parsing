4a40d4abb1b3e3445af1c41255d5dce31cac120f
WiseBoiii <aleksashin.1001@gmail.com>
2024-08-03 16:54:36 +0300

    Now parser is ready to handle network issues

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,6 +4,7 @@ import urllib
 import urllib3
 from core_functions import parse_book_page, download_image, download_txt
 import json
+import time
 import argparse
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -31,38 +32,45 @@ def main():
     fantastic_args = fantastic_parser.parse_args()
     for page in range(fantastic_args.start_page, fantastic_args.end_page):
         url = f'{url_pattern}{page}'
-        page_response = requests.get(url)
-        page_response.raise_for_status()
-        soup = BeautifulSoup(page_response.text, 'lxml')
-        all_fantastic_book_ids = soup.select('.d_book')
-        for fantastic_book in all_fantastic_book_ids:
-            fantastic_book_id = fantastic_book.select_one('a')['href']
-            param_id = int(fantastic_book_id.replace('/', '').lstrip('b'))
-            params = {
-                'id': param_id
-            }
-            fantastic_book_link = urllib.parse.urljoin(book_url_pattern, fantastic_book_id)
-            fantastic_book_response = requests.get(fantastic_book_link)
-            fantastic_book_response.raise_for_status()
-            parsed_fantastic_book = parse_book_page(fantastic_book_link, fantastic_book_response)
-            if not fantastic_args.skip_imgs:
-                download_image(parsed_fantastic_book['image'], fantastic_args.dest_folder)
-            if not fantastic_args.skip_txt:
-                downloaded_fantastic_book_response = requests.get(downloading_url, params=params)
-                downloaded_fantastic_book_response.raise_for_status()
-                download_txt(downloaded_fantastic_book_response, parsed_fantastic_book['title'],
-                             fantastic_args.dest_folder)
-            fantastic_book_img_src = f"previews/{parsed_fantastic_book['image'].split('/')[-1]}"
-            fantastic_book_path = f"books/{parsed_fantastic_book['title']}.txt"
-            extended_parsed_fantastic_book = {
-                'title': parsed_fantastic_book['title'],
-                'author': parsed_fantastic_book['author'],
-                'img_src': fantastic_book_img_src,
-                'book_path': fantastic_book_path,
-                'comments': parsed_fantastic_book['comments'],
-                'genres': parsed_fantastic_book['genres']
-            }
-            fantastic_books.append(extended_parsed_fantastic_book)
+        try:
+            page_response = requests.get(url)
+            page_response.raise_for_status()
+            soup = BeautifulSoup(page_response.text, 'lxml')
+            all_fantastic_book_ids = soup.select('.d_book')
+            for fantastic_book in all_fantastic_book_ids:
+                fantastic_book_id = fantastic_book.select_one('a')['href']
+                param_id = int(fantastic_book_id.replace('/', '').lstrip('b'))
+                params = {
+                    'id': param_id
+                }
+                fantastic_book_link = urllib.parse.urljoin(book_url_pattern, fantastic_book_id)
+                fantastic_book_response = requests.get(fantastic_book_link)
+                fantastic_book_response.raise_for_status()
+                parsed_fantastic_book = parse_book_page(fantastic_book_link, fantastic_book_response)
+                if not fantastic_args.skip_imgs:
+                    download_image(parsed_fantastic_book['image'], fantastic_args.dest_folder)
+                if not fantastic_args.skip_txt:
+                    downloaded_fantastic_book_response = requests.get(downloading_url, params=params)
+                    downloaded_fantastic_book_response.raise_for_status()
+                    download_txt(downloaded_fantastic_book_response, parsed_fantastic_book['title'],
+                                 fantastic_args.dest_folder)
+                fantastic_book_img_src = f"previews/{parsed_fantastic_book['image'].split('/')[-1]}"
+                fantastic_book_path = f"books/{parsed_fantastic_book['title']}.txt"
+                extended_parsed_fantastic_book = {
+                    'title': parsed_fantastic_book['title'],
+                    'author': parsed_fantastic_book['author'],
+                    'img_src': fantastic_book_img_src,
+                    'book_path': fantastic_book_path,
+                    'comments': parsed_fantastic_book['comments'],
+                    'genres': parsed_fantastic_book['genres']
+                }
+                fantastic_books.append(extended_parsed_fantastic_book)
+        except requests.exceptions.HTTPError:
+            print('Такой книги не существует')
+        except requests.ConnectionError:
+            print('Проблемы с соединением. Идет переподключение...')
+            time.sleep(5)
+
     all_about_fantastic_books = json.dumps(fantastic_books, ensure_ascii=False).encode('utf-8')
     with open(f'{fantastic_args.dest_folder}/all_about_fantastic_books.json', 'wb') as fantastic_book_file:
         fantastic_book_file.write(all_about_fantastic_books)
